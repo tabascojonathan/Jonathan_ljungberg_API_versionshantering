fdd716fe8963a74bde0fe0e38353e53e51b40511
jonathan <jonathan.ljungberg96@gmail.com>
2024-05-29 15:32:25 +0200

    SPA setup
    Installerat react med en SPA för API:et foxes

---
+++ foxes_api/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import FoxGallery from './FoxGallery';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Fox Gallery</h1>
+      <FoxGallery />
+    </div>
+  );
+}
+
+export default App;
